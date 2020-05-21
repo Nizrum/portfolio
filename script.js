a0524697aae728d1a94a820dda707416accb9ada
Nizrum <62435919+Nizrum@users.noreply.github.com>
2020-05-21 20:44:21 +0400

    Добавлены файлы
    Добавлены файлы

---
+++ script.js
@@ -0,0 +1,16 @@
+var mySwiper = new Swiper ('.swiper-container', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 1,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      1480: {
+        slidesPerView: 2
+      }
+    }
+  })
